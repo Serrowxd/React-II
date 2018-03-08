a2d4aa7c76a52a9d2015c29a7e971ecb6dd28c07
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-08 15:02:02 -0600

    Fresh Start
    Starting from scratch, all empty files.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,29 +4,5 @@ import './App.css';
 import CommentSection from './components/CommentSection/CommentSection.js';
 import PostContainer from './components/PostContainer/PostContainer.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
-import { dummyData } from './dummy-data.js';
+import dummyData from './dummy-data';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dumArr: [],
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ dumArr: dummyData});
-  }
-
-  render() {
-    return (
-      <div className="App">
-      <SearchBar />
-      <PostContainer dummyData={this.state.dumArr} />
-      <CommentSection />
-      </div>
-    );
-  }
-}
-
-export default App;
